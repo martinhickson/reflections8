db204804cec736ca41621b134f41e317a4b1573a
Andreas Schörk <andreas.schoerk@1und1.de>
2018-08-13 15:33:00 +0200

    introduce staging repo

--- src/test/java/org/reflections/MyTestModelStore.java
+++ src/test/java/org/reflections/MyTestModelStore.java
@@ -1,4 +1,4 @@
-//generated using Reflections JavaCodeSerializer [Mon Aug 13 15:07:28 CEST 2018]
+//generated using Reflections JavaCodeSerializer [Mon Aug 13 15:28:10 CEST 2018]
 package org.reflections;
 
 public interface MyTestModelStore {
